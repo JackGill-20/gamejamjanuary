e181a64f2bb8cf19cfd384cb40d17dd7eb70a16c
u1310931 <u1310931@utah.edu>
2023-01-21 12:23:13 -0700

    Camera Movement functional

--- Assets/_Scripts/Camera/CameraMovement.cs
+++ Assets/_Scripts/Camera/CameraMovement.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
-    public float playerPosition = default;
-    public float height = default;  //Change to take height of camera
-    public float angle = 65;
+    public GameObject player = default;
+
+    public float height = default;
+    public float angle = 25;
+
+    public float heightOffset = default;
+    public float offset = default;
 
     //Finds the position the camera needs to keep the player centered
     //Distance from player equation is as follows:
     //  height * tan(angle) = distance
 
+    void Start()
+    {
+        height = transform.position.y;
+    }
+
     void FixedUpdate()
     {
-        
+
+        heightOffset = height - player.transform.position.y;
+        offset = heightOffset * Mathf.Tan((Mathf.PI/180) * angle);
+
+        transform.position = new Vector3(player.transform.position.x, height, player.transform.position.z - offset);
+
     }
 }
