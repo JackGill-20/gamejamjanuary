ef67d09764d3bb03445b22ec453dba1a62e511d2
u1310931 <u1310931@utah.edu>
2023-01-21 13:10:27 -0700

    Ghost follow mechanic functional

--- Assets/_Scripts/Ghost Scripts/Ghost.cs
+++ Assets/_Scripts/Ghost Scripts/Ghost.cs
@@ -16,20 +16,42 @@ public class Ghost : MonoBehaviour
     //  Pushes boxes when colliding, pushes until the box hits a wall, then acts like wall
     //  Float over water
 
-    public bool commanded = false;
+    public GameObject player = default;
+
+    private Rigidbody rb;
+
+    public float moveSpeed = 5f;
+    public Vector2 moveDirection = default;
+    public bool directed = false;
+
+    public float followRadius = 2;
 
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+        moveDirection = new Vector2(0, 0);
     }
 
     void FixedUpdate()
     {
 
-        if (!commanded)
+        if (!directed)
         {
             //Follow player movement
+            if(transform.position.x > player.transform.position.x + followRadius || transform.position.x < player.transform.position.x - followRadius ||
+                transform.position.z > player.transform.position.z + followRadius || transform.position.z < player.transform.position.z - followRadius)
+            {
+                Vector3 direction = player.transform.position - transform.position;
+                direction = direction.normalized;
+                moveDirection.x = direction.x;
+                moveDirection.y = direction.z;
+            }
+            else
+            {
+                moveDirection = Vector2.zero;
+            }
 
+            rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, moveDirection.y * moveSpeed);
         }
         else
         {
