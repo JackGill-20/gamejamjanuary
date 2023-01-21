2b253db474ffb4097549328c7a426e9f07e14da4
Jack Gill <58401205+JackGill-20@users.noreply.github.com>
2023-01-20 19:20:26 -0700

    level
    basic level

--- Assets/_Scripts/Player Scripts/PlayerController.cs
+++ Assets/_Scripts/Player Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class newscript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
